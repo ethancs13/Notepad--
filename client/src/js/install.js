4d288c3d7fb00846dd49e7f8f9b623fc69511b47
Ethan Sroka <ethansroka@gmail.com>
2024-02-06 20:14:20 -0700

    event listeners for install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,36 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Saves event in global variable.
+    // store for later
+    window.deferredPrompt = event;
+
+    // .toggle() method removes class depending on state
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    // if prompt empty, return
+    if (!promptEvent) {
+        return;
+    }
+
+    // prompt user to install
+    promptEvent.prompt();
+
+    // set deffered prompt to null / reset
+    window.deferredPrompt = null;
+
+    // hide
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    // set deferredPrompt property to null to clean up global vars
+    window.deferredPrompt = null;
+});
